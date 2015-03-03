62afaa1df023eb8ebe9065cedcf341cd37e0e89c
Christopher Howse <cdh682@mun.ca>
2015-03-03 10:19:41 -0330

    Inital styling for admin page

--- src/posts/PostManager.java
+++ src/posts/PostManager.java
@@ -1,5 +1,7 @@
 package posts;
 
+import utilityDtos.BooleanDto;
+
 import java.util.ArrayList;
 
 public class PostManager
@@ -9,8 +11,16 @@ public class PostManager
         ArrayList<Post> result = new ArrayList<Post>();
         //TODO: Get posts from database;
         //for now fake posts
-        result.add(new Post("username", "post body"));
-        result.add(new Post("username", "post body 2"));
+        result.add(new Post("test username", 1, "post body"));
+        result.add(new Post("test username", 2, "post body 2"));
+        return result;
+    }
+
+    public static BooleanDto deletePost(int postId)
+    {
+        BooleanDto result = new BooleanDto(false);
+        //TODO: delete post from database here
+        result.success = true;
         return result;
     }
 }
--- src/users/User.java
+++ src/users/User.java
@@ -1,13 +1,36 @@
 package users;
 
-public class User {
+public class User implements Comparable
+{
     public final String name;
     public final String password;
-    public final String role;
+    public String role;
 
     public User( String n, String p, String r ) {
         name = n;
         password = p;
         role = r;
+    }
+
+    public void toggleRole()
+    {
+        if(this.role.equals("admin"))
+        {
+            this.role = "pleb";
+        }
+        else
+        {
+            this.role = "admin";
+        }
+    }
+
+    @Override
+    public int compareTo(Object o)
+    {
+        if(o instanceof User)
+        {
+            if(this.name.equals(((User) o).name)) return 0;
+        }
+        return -1;
     }
 }
