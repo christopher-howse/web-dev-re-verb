594f5bcbaefbc52cf68860dd7f1c7b27ac898b49
jacobbrown0192 <jacobbrown0192@gmail.com>
2015-03-20 14:54:02 -0230

    change to some header stuff

--- static/navigation.js
+++ static/navigation.js
@@ -1,50 +1,24 @@
 function init()
 {
-var navBar = document.querySelector('.reverb-navigation-bar');
-      var headerBar = document.querySelector('.reverb-header-bar');
-      var navBg = document.querySelector('.reverb-navigation-bg');
+    var navBar = document.querySelector('.reverb-navigation-bar');
+    var headerBar = document.querySelector('.reverb-header-bar');
+    var navBg = document.querySelector('.reverb-navigation-bg');
 
-      var menuBtn = document.querySelector('.reverb-hamburger');
-      menuBtn.addEventListener('click', function() 
-        {
+    var menuBtn = document.querySelector('.reverb-hamburger');
+    menuBtn.addEventListener('click', function() 
+    {
         var isOpen = navBar.classList.contains('open');
         if(isOpen) 
         {
-          headerBar.classList.remove('open');
-          navBar.classList.remove('open');
-          navBg.classList.remove('open');
+            headerBar.classList.remove('open');
+            navBar.classList.remove('open');
+            navBg.classList.remove('open');
         } 
         else 
         {
-          headerBar.classList.add('open');
-          navBar.classList.add('open');
-          navBg.classList.add('open');
+            headerBar.classList.add('open');
+            navBar.classList.add('open');
+            navBg.classList.add('open');
         }
-      }, true);
+    }, true);
 }
-
-function init2()
-{
-var navdrawerContainer = document.querySelector('.navdrawer-container');
-      var appbarElement = document.querySelector('.app-bar');
-      var darkbgElement = document.querySelector('.navdrawer-bg');
-
-      var menuBtn = document.querySelector('.menu');
-      menuBtn.addEventListener('click', function() {
-        var isOpen = navdrawerContainer.classList.contains('open');
-        if(isOpen) {
-          appbarElement.classList.remove('open');
-          navdrawerContainer.classList.remove('open');
-          darkbgElement.classList.remove('open');
-        } else {
-          appbarElement.classList.add('open');
-          navdrawerContainer.classList.add('open');
-          darkbgElement.classList.add('open');
-        }
-      }, true);
-}
-
-//        var menuBtn = document.querySelector('.reverb-hamburger');
-//      menuBtn.addEventListener('click', function() {
-//        sampleCompleted('appbar-navdrawer-bottombar-sample.html-menuclick');
-//      }, true);
