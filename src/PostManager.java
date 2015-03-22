73975ee75fed7544cc6180fa036032a62332f068
jacobbrown0192 <jacobbrown0192@gmail.com>
2015-03-22 13:01:47 -0230

    starting to get messages

--- src/PostManager.java
+++ src/PostManager.java
@@ -15,6 +15,22 @@ public class PostManager
     private static String selectByUsername =
             "SELECT * FROM Messages WHERE username = ?";
 
+    private static String selectByLocation =
+            "SELECT " +
+            "message_id, user_id, message_body, anon_flag, location, report_count, vote_count, reply_link, create_time,  (" +
+        "6371 * acos (" +
+        "cos ( radians(?) )" + //lat
+        "* cos( radians( X(location) ) )" +
+        "* cos( radians( Y(location) ) - radians(?) )" + //long
+        "+ sin ( radians(?) )" + //lat
+        "* sin( radians( X(location) ) )" +
+        ")" +
+        ") AS distance" +
+    "FROM Messages" +
+    "HAVING distance < 1 AND UNIX_TIMESTAMP(create_time) <= UNIX_TIMESTAMP(?) and Reply_link is NULL" + //time
+    "ORDER BY create_time desc,distance"; //+
+//    "LIMIT 25;";
+
     private static String insertIntoMessages =
             "INSERT INTO Messages VALUES(NULL, ?, ?, ?," +
                     "?, ?, ?, ?, ?)";
@@ -55,6 +71,32 @@ public class PostManager
         }
 
         return result;
+    }
+
+    public ArrayList<Post> getPostsByLocation(String lat,String lon,String time)
+    {
+        ArrayList<Post> result = new ArrayList<Post>();
+        try(
+                Connection conn = DriverManager.getConnection(DatabaseManager.dbURL);
+                PreparedStatement stmt = conn.prepareStatement( selectByLocation );
+        ) {
+            stmt.setQueryTimeout(DatabaseManager.timeout);
+            stmt.setString(1, lat);
+            stmt.setString(2, lon);
+            stmt.setString(3, lat);
+            stmt.setString(4, time);
+            ResultSet rs = stmt.executeQuery();
+            while (rs.next())
+            {
+                result.add(new Post(rs.getString("username"), rs.getInt("message_id"), rs.getString("message_body")));
+            }
+        } catch (SQLException e)
+        {
+            System.out.println("Could not get posts at: " + lat + "," + lon + "," + time);
+        }
+
+        return result;
+
     }
 
     public static BooleanDto deleteMessage(int messageId)
---
+++ src/PostSparkCalls.java
@@ -0,0 +1,54 @@
+import com.google.gson.Gson;
+import error.Error;
+import posts.*;
+import static spark.Spark.*;
+import spark.Session;
+import users.User;
+
+import java.util.Date;
+
+/**
+ * Created by Jake on 19/03/2015.
+ */
+public class PostSparkCalls
+{
+    private DatabaseManager databaseManager;
+
+    public PostSparkCalls(DatabaseManager databaseManager)
+    {
+        this.databaseManager = databaseManager;
+        posting();
+    }
+
+    public void posting()
+    {
+        post("/posting", (request, response) ->
+        {
+            Session sess = request.session(true);
+            if ( sess == null )
+            {
+                return Error.errorPage("failed to get the session");
+            }
+            String postBody = request.queryParams("post");
+            User user = request.session().attribute("user");
+
+            //TODO: Add the post using post manager
+
+            response.redirect("/feed.html");
+
+            return null;
+        });
+
+        post("/getMessagesByLocation", "application/json", (request,response) ->
+        {
+//            Gson gson = new Gson();
+            response.type("application/json");
+            String lat = "0";
+            String lon = "0";
+            Date now = new Date();
+            long unix = now.getTime()/1000;
+            String time = String.valueOf(unix);
+            return databaseManager.getPostMan().getPostsByLocation(lat,lon,time);
+        }, new JsonTransformer());
+    }
+}
