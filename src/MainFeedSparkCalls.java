162f044ee9b20d2b830c282ff8593fbfa470cd46
= <=>
2015-03-28 15:30:33 -0230

    Anonymous posting is now working, but the message should probable be changed to look better

--- src/MainFeedSparkCalls.java
+++ src/MainFeedSparkCalls.java
@@ -31,5 +31,21 @@ public class MainFeedSparkCalls
             request.session().attribute("longitude", longitude);
             return ((User)request.session().attribute("user")).name;
         });
+
+        get("/getIsAnonymous", "application/x-www-form-urlencoded", (request, response) ->
+        {
+            response.type("application/application/x-www-form-urlencoded");
+            boolean anon = request.session().attribute("anon");
+            System.out.println("Getting anon " + Boolean.toString(anon));
+            return Boolean.toString(anon);
+        });
+
+        post("/setAnonymous", (request, response) ->
+        {
+            boolean anon = Boolean.parseBoolean(request.queryParams("anon"));
+            System.out.println("Setting anon " + request.queryParams("anon"));
+            request.session().attribute("anon", anon);
+            return " ";
+        });
     }
 }
